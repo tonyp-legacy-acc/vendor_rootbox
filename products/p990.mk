6714349af5b0da7f726a54ee28501d5a06c628f4
tonyp <text2tony@gmail.com>
2013-01-10 04:19:51 +0100

    add fingerprint, skip vendor/cm

--- products/p990.mk
+++ products/p990.mk
@@ -1,5 +1,5 @@
 # Inherit CM device configuration
-$(call inherit-product, device/lge/p990/cm.mk)
+$(call inherit-product, device/lge/p990/p990.mk)
 
 # Inherit AOKP common bits
 $(call inherit-product, vendor/rootbox/configs/common.mk)
@@ -18,6 +18,10 @@ PRODUCT_DEVICE := p990
 PRODUCT_NAME := rootbox_p990_tonyp
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-P990
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=star_open_eu BUILD_ID=IMM76L BUILD_FINGERPRINT=lge/star_open_eu/star:4.0.4/IMM76L/LG-P990-P99030a.1211302332:user/release-keys PRIVATE_BUILD_DESC="star_open_eu-user 4.0.4 IMM76L LG-P990-P99030a.1211302332 release-keys"
+PRODUCT_RELEASE_NAME := Optimus2X
 
 # Copy maguro specific prebuilt files
 PRODUCT_COPY_FILES += \
