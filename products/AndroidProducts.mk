814382fd408917f836c56c7fa924042f21286c7f
tonyp <text2tony@gmail.com>
2013-01-10 05:23:44 +0100

    I'm only building for the P990 anyways

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,8 +1,1 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/d2att.mk \
-$(LOCAL_DIR)/grouper.mk \
-$(LOCAL_DIR)/i9100.mk \
-$(LOCAL_DIR)/i9100g.mk \
-$(LOCAL_DIR)/i9100p.mk \
-$(LOCAL_DIR)/i9300.mk \
-$(LOCAL_DIR)/p990.mk \
-$(LOCAL_DIR)/n7000.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/p990.mk
